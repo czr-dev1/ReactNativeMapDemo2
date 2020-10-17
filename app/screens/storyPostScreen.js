f3aa7b028f01bbb56bd51700115aadc6e36d163a
Carlos L <carloslariossolis@gmail.com>
2020-10-17 14:47:05 -0700

    Post stories from basic story creation screen

--- app/screens/storyPostScreen.js
+++ app/screens/storyPostScreen.js
@@ -4,22 +4,32 @@ import { StyleSheet, Text, Platform, ActivityIndicator, SafeAreaView, StatusBar,
   TouchableWithoutFeedback, Keyboard } from 'react-native';
 import * as Location from 'expo-location';
 import DateTimePicker from '@react-native-community/datetimepicker';
+import axios from 'axios';
 
 import colors from '../config/colors';
 
 function StoryPostScreen(props) {
-  const [gotLocation, setGotLocation] = useState(false);
-  const [location, setLocation] = useState({});
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
+  // These are here so you know what to include in your post request
   const [address, setAddress] = useState('');
-  const [locality, setLocality] = useState('');
+  const [anonradius, setAnonRadius] = useState(1)
+  const [category, setCategory] = useState(1);
   const [country, setCountry] = useState('');
-  const [postCode, setPostCode] = useState('');
-  const [region, setRegion] = useState('');
-  const [category, setCategory] = useState(0);
-  const [startDate, setStartDate] = useState({});
+  const [description, setDescription] = useState('');
   const [endDate, setEndDate] = useState({});
+  const [isAnonymous, setAnonymous] = useState(true);
+  const [lastEditDate, setLastEditDate] = useState({});
+  const [lastPersonEdit, setLastPersonEdit] = useState('');
+  const [location, setLocation] = useState({}); //make sure to split to latitude and longitude
+  const [locality, setLocality] = useState('');
+  const [owner, setOwner] = useState('');
+  const [postCode, setPostCode] = useState('');
+  const [postDate, setPostDate] = useState('');
+  const [region, setRegion] = useState('');
+  const [startDate, setStartDate] = useState({});
+  const [title, setTitle] = useState('');
+
+  const [gotLocation, setGotLocation] = useState(false);
+
 
 
   useEffect(() => {
@@ -41,6 +51,63 @@ function StoryPostScreen(props) {
       console.log(location);
     }
   };
+
+  const submitNewStory = async () => {
+    const latSplit = location.latitude.toString().split('.');
+    const lonSplit = location.longitude.toString().split('.');
+    const latitude = latSplit[0] + '.' + latSplit[1].substring(0,6);
+    const longitude = lonSplit[0] + '.' + lonSplit[1].substring(0,6);
+
+    const pin = {
+      address: address,
+      anonradius: anonradius,
+      category: category,
+      country: country,
+      description: description,
+      endDate: new Date(),
+      is_anonymous_pin: true,
+      lastEditDate: new Date(),
+      lastPersonEdit: null,
+      latitude: latitude,
+      longitude: longitude,
+      locality: locality,
+      owner: owner,
+      postCode: postCode,
+      postDate: new Date(),
+      region: region,
+      startDate: new Date(),
+      title: title
+    }
+    console.log(pin);
+
+    const config = {
+      headers: {
+        "X-Arqive-Api-Key": "4BqxMFdJ.3caXcBkTUuLWpGrfbBDQYfIyBVKiEif1",
+      },
+    };
+
+    axios.post('http://www.globaltraqsdev.com/api/pins/', pin, config)
+    .then((res) => {
+      console.log(res.data);
+    }).catch((err) => {
+      console.log(err);
+    });
+
+    try{
+      let response = await fetch('http://www.globaltraqsdev.com/api/pins', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(pin)
+      });
+      let json = await response.json();
+      console.log(json);
+    } catch (error) {
+      console.log(error);
+    }
+
+  }
 
 
   return (
@@ -107,7 +174,10 @@ function StoryPostScreen(props) {
           onChangeText={val => {setDescription(val)}}
         />
 
-        <Button disabled={gotLocation ? false : true} title="Submit"/>
+        <Button
+          disabled={gotLocation ? false : true}
+          title="Submit"
+          onPress={(e) => {submitNewStory();}}/>
       </View>
       </TouchableWithoutFeedback>
     </SafeAreaView>
