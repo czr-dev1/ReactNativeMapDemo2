49e27175241e3ecd670fd20c641cb011d207ecdc
Carlos L <carloslariossolis@gmail.com>
2020-10-26 10:33:45 -0700

    Enhance tab navigation with smaller navigation

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import {createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
 import { FontAwesome5 } from '@expo/vector-icons';
 
@@ -10,8 +10,27 @@ import ProfileScreen from './app/screens/profileScreen';
 import StoryScreen from './app/screens/storyScreen';
 import StoryPostScreen from './app/screens/storyPostScreen';
 
-//const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
+
+const StoriesStack = createStackNavigator();
+function StoriesStackScreen() {
+  return (
+    <StoriesStack.Navigator>
+      <StoriesStack.Screen name="Profile" component={ ProfileScreen } />
+      <StoriesStack.Screen name ="Story" component={ StoryScreen } />
+    </StoriesStack.Navigator>
+  );
+}
+
+const MapStack = createStackNavigator();
+function MapStackScreen() {
+  return (
+    <MapStack.Navigator>
+      <MapStack.Screen name="Map" component={ MapScreen } />
+      <MapStack.Screen name="Story" component={ StoryScreen } />
+    </MapStack.Navigator>
+  );
+}
 
 export default function App() {
   return (
@@ -30,10 +49,9 @@ export default function App() {
             return <FontAwesome5 name={iconName} size={size} color={color} />
           }
         })}>
-        <Tab.Screen name="Map" options={{header: () => null}} component={MapScreen} />
-        <Tab.Screen name="Post" options={{header: () => null}} component={StoryPostScreen} />
-        <Tab.Screen name="Profile" component={ProfileScreen} />
-        <Tab.Screen name="Story" component={StoryScreen} />
+        <Tab.Screen name="Map" options={{header: () => null}} component={ MapStackScreen } />
+        <Tab.Screen name="Post" options={{header: () => null}} component={ StoryPostScreen } />
+        <Tab.Screen name="Profile" component={StoriesStackScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
