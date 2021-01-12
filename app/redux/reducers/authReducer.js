dac8b5dda7b6c0fe6c3f9497a5fbb576cf05e45a
cpahed <55155486+cpahed@users.noreply.github.com>
2021-01-12 10:44:44 -0800

    Update authReducer.js

--- app/redux/reducers/authReducer.js
+++ app/redux/reducers/authReducer.js
@@ -9,23 +9,16 @@ let initialState = {
 
 export default login = (state = initialState, action) => {
     switch (action.type) {
-        case 'LOGIN_USER_LOADING':
-            console.log('loading user');
-            return Object.assign({}, state, { isLoading: true });
         case 'LOGIN_USER_SUCCESS':
-            console.log('successfully logged in');
-            return Object.assign({}, state, {
-              token: action.payload.token, 
-              username: action.payload.user.username,
-              bio: action.payload.user.bio,
-              userStories: action.payload.user.userStories,
-              bookmarkedStories: action.payload.user.user_upvoted_stories,
-              isLoggedIn: true,
-              isAuthenticated: true,
-              isLoading: false,
-              loginFail: false,
-              guest_user: false 
-            });
+			console.log('LOGIN SUCCESS!');
+			return Object.assign({}, state, {
+				token: action.payload.token,
+				isLoggedIn: true,
+				isAuthenticated: true,
+				isLoading: false,
+				loginFail: false,
+				guest_user: false,
+			});
         case 'LOGIN_USER_FAIL':
             console.log('failed to login');
             return Object.assign({}, state, { loginFail: true, guest_user: true });
@@ -36,6 +29,19 @@ export default login = (state = initialState, action) => {
               isAuthenticated: false, 
               guest_user: true 
             });
+        case 'REGISTER_USER_SUCCESS':
+			console.log('USER REGISTERED!');
+			return Object.assign({}, state, {
+				token: action.payload.token,
+				isLoggedIn: true,
+				isAuthenticated: true,
+				isLoading: false,
+				loginFail: false,
+				guest_user: false,
+			});
+		case 'REGISTER_USER_FAIL':
+			console.log('FAILED TO REGISTER USER!');
+			return Object.assign({}, state, { loginFail: true, guest_user: true });
         default:
             return state
     }
