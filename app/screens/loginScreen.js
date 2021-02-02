65256d45e1d1cc11f11954aa0ca6ddcb85106e26
cpahed <55155486+cpahed@users.noreply.github.com>
2021-02-02 13:02:19 -0800

    Update loginScreen.js

--- app/screens/loginScreen.js
+++ app/screens/loginScreen.js
@@ -1,66 +1,154 @@
 import React, { useState } from 'react';
-import { 
-  Button, 
-  SafeAreaView, 
-  StyleSheet, 
-  Text, 
-  TextInput 
+import {
+	Button,
+	Image,
+	SafeAreaView,
+	StyleSheet,
+	Text,
+	TextInput,
+	TouchableWithoutFeedback,
+	View,
 } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { useNavigation } from '@react-navigation/native';
 import { useDispatch } from 'react-redux';
-import { login } from '../redux/actions/authActions';
+import * as Animatable from 'react-native-animatable';
 
+import { login } from '../redux/actions/authActions';
 import colors from '../config/colors';
+import { Alert } from 'react-native';
 
 function LoginScreen() {
 	const dispatch = useDispatch();
 	const navigation = useNavigation();
 
-	const [username, setUsername] = useState('');
-	const [password, setPassword] = useState('');
+	const [user, setUser] = useState({
+		username: '',
+		password: '',
+		isValidUser: true,
+		isValidPassword: true,
+		secureTextEntry: true,
+		errors: {},
+	});
 
-	const user = {
-		username: username,
-		password: password,
+	const handleUsername = (val) => {
+		if (val === '') {
+			setUser({
+				...user,
+				username: val,
+				isValidUser: false,
+			});
+		} else {
+			setUser({
+				...user,
+				username: val,
+				isValidUser: true,
+			});
+		}
+	};
+
+	const handlePassword = (val) => {
+		if (val.length >= 8) {
+			setUser({
+				...user,
+				password: val,
+				isValidPassword: true,
+			});
+		} else {
+			setUser({
+				...user,
+				password: val,
+				isValidPassword: false,
+			});
+		}
+	};
+
+	const updateSecureTextEntry = () => {
+		setUser({
+			...user,
+			secureTextEntry: !user.secureTextEntry,
+		});
+	};
+
+	const errMsg = () => {
+		Alert.alert('', 'invalid username and/or password, please try again', [{ text: 'ok' }]);
 	};
 
 	return (
 		<SafeAreaView style={styles.container} forceInset={{ top: 'always' }}>
-			<Text>Username</Text>
-      <TextInput 
-        value={username} 
-        style={styles.input} 
-        onChangeText={(val) => setUsername(val)} 
-      />
-			<Text>Password</Text>
+			<Image style={styles.logo} source={require('../assets/thearqive_bubbles.png')} />
+			<Text style={styles.title}>log in</Text>
+
 			<TextInput
-				value={password}
 				style={styles.input}
-				onChangeText={(val) => setPassword(val)}
-				secureTextEntry={true}
+				value={user.username}
+				placeholder='username'
+				autoCapitalize='none'
+				autoCorrec={false}
+				onChangeText={(val) => handleUsername(val)}
 			/>
+			{user.isValidUser ? null : (
+				<Animatable.View animation='fadeInLeft' duration={500}>
+					<Text style={styles.errorMsg}>please enter your username</Text>
+				</Animatable.View>
+			)}
 
-			<Button
-				title='Login'
-				onPress={() => dispatch(login(user))}
+			<TextInput
+				style={styles.input}
+				value={user.password}
+				placeholder='password'
+				autoCapitalize='none'
+				autoCorrect={false}
+				secureTextEntry={user.secureTextEntry ? true : false}
+				onChangeText={(val) => handlePassword(val)}
 			/>
+			{user.isValidPassword ? null : (
+				<Animatable.View animation='fadeInLeft' duration={500}>
+					<Text style={styles.errorMsg}>password must be at least 8 characters long</Text>
+				</Animatable.View>
+			)}
 
-			<Text style={styles.text}>Don't have an account?</Text>
-			<TouchableOpacity
-				onPress={() => {
-					navigation.navigate('Register');
-				}}
-			>
-				<Text style={styles.userBtn}>Register</Text>
-			</TouchableOpacity>
-			<TouchableOpacity
-				onPress={() => {
-					navigation.navigate('Map');
-				}}
-			>
-				<Text style={styles.text}>Continue as Anonymous</Text>
-			</TouchableOpacity>
+			<View style={styles.links}>
+				<Text style={{ color: 'gray' }}>forgot your log in details? get help </Text>
+				<TouchableOpacity
+					onPress={() => {
+						navigation.navigate('');
+					}}
+				>
+					<Text style={{ fontWeight: 'bold', color: '#696969' }}>signing in</Text>
+				</TouchableOpacity>
+			</View>
+
+			<View style={styles.loginBtn}>
+				<Button
+					title='log in'
+					color='white'
+					onPress={() => {
+						dispatch(login(user));
+					}}
+				/>
+			</View>
+
+			<View style={styles.links}>
+				<Text style={{ color: 'gray' }}>don't have an account? register </Text>
+				<TouchableOpacity
+					onPress={() => {
+						navigation.navigate('Register');
+					}}
+				>
+					<Text style={{ fontWeight: 'bold', color: '#696969' }}>here</Text>
+				</TouchableOpacity>
+			</View>
+
+			<View>
+				<TouchableWithoutFeedback
+					onPress={() => {
+						navigation.navigate('Map');
+					}}
+				>
+					<Text style={styles.text}>continue</Text>
+				</TouchableWithoutFeedback>
+			</View>
 		</SafeAreaView>
 	);
 }
@@ -72,22 +160,57 @@ const styles = StyleSheet.create({
 		alignItems: 'center',
 		justifyContent: 'center',
 	},
-	input: {
-		borderWidth: 1,
-		borderColor: '#ddd',
-		borderRadius: 6,
-		padding: 10,
-		fontSize: 10,
-		width: '80%',
+	logo: {
 		marginBottom: 10,
+	},
+	title: {
+		fontSize: 24,
+		fontWeight: 'bold',
+		color: '#A9A9A9',
+		marginBottom: 50,
+	},
+	body: {
+		flexDirection: 'row',
+	},
+	input: {
+		fontSize: 20,
+		color: 'gray',
+		width: '85%',
+		height: 50,
+		paddingLeft: 15,
+		borderWidth: 2,
+		borderRadius: 5,
+		borderColor: '#DCDCDC',
+		marginTop: 10,
+		marginBottom: 10,
+	},
+	icon: {
+		alignItems: 'flex-end',
+		position: 'absolute',
 	},
 	text: {
 		fontSize: 14,
-		marginTop: 15,
+		color: 'gray',
+		marginTop: 190,
 	},
-	userBtn: {
-		fontSize: 16,
-		color: 'purple',
+	errorMsg: {
+		color: 'red',
+	},
+	loginBtn: {
+		fontSize: 18,
+		width: '85%',
+		padding: 10,
+		borderRadius: 5,
+		backgroundColor: '#DCDCDC',
+		marginTop: 50,
+		marginBottom: 60,
+	},
+	links: {
+		flexDirection: 'row',
+		justifyContent: 'flex-start',
+		fontSize: 14,
+		marginTop: 10,
+		marginBottom: 30,
 	},
 });
 
