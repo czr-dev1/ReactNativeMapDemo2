3d7420554b7420fe277522e5699cb2ae4f390988
cpahed <55155486+cpahed@users.noreply.github.com>
2021-02-02 20:54:03 -0800

    updated actions

--- app/redux/actions/authActions.js
+++ app/redux/actions/authActions.js
@@ -1,62 +1,64 @@
 import axios from 'axios';
 
 const config = {
-  headers: {
-    'X-Arqive-Api-Key': '4BqxMFdJ.3caXcBkTUuLWpGrfbBDQYfIyBVKiEif1',
-  },
+	headers: {
+		'X-Arqive-Api-Key': '4BqxMFdJ.3caXcBkTUuLWpGrfbBDQYfIyBVKiEif1',
+	},
 };
 
-export const tokenConfig = (getState) => {
-  const token = getState().authReducer.token;
-  const config = {
-    headers: {
-      'X-Arqive-Api-Key': '4BqxMFdJ.3caXcBkTUuLWpGrfbBDQYfIyBVKiEif1',
-    },
-  };
-
-  if (token) {
-    config.headers['Authorization'] = `Token ${token}`;
-  }
-  return config;
+// LOAD ALL USER
+export const loadUsers = () => {
+	return (dispatch) => {
+		axios
+			.get('https://www.globaltraqsdev.com/auth/users', config)
+			.then((res) => {
+				dispatch({ type: 'LOAD_USERS', payload: res.data});
+			})
+			.catch ();
+	}
 };
 
-export const login = ({username, password}) => {
-  return (dispatch) => {
-    const user = {
-      username: username,
-      password: password
-    }
+// LOGIN USER
+export const login = ({ username, password }) => {
+	return (dispatch) => {
+		const user = {
+			username: username,
+			password: password,
+		};
 
-    axios.post('https://www.globaltraqsdev.com/api/auth/login', user, config)
-    .then((res) => {
-      dispatch({ type: 'LOGIN_USER_SUCCESS', payload: res.data });
-      dispatch({ type: 'LOAD_PROFILE_SUCCESS', payload: res.data });
-    })
-    .catch((err) => {
-      dispatch({ type: 'LOGIN_USER_FAIL', payload: err.response.data });
-    });
-  }
+		axios
+			.post('https://www.globaltraqsdev.com/api/auth/login', user, config)
+			.then((res) => {
+				dispatch({ type: 'LOGIN_USER_SUCCESS', payload: res.data });
+				dispatch({ type: 'LOAD_PROFILE_SUCCESS', payload: res.data });
+			})
+			.catch((err) => {
+				dispatch({ type: 'LOGIN_USER_FAIL' });
+			});
+	};
 };
 
+// LOGOUT USER
 export const logout = () => {
-  return (dispatch, getState) => {
-    axios.post('https://www.globaltraqsdev.com/api/auth/logout', null, tokenConfig(getState))
-    .then((res) => {
-      dispatch({ type: 'LOGOUT_USER_SUCCESS' });
-    })
-    .catch((err) => {
-      dispatch(returnErrors(err.response.data, err.response.status));
-    });
-  }
+	return (dispatch, getState) => {
+		axios
+			.post('https://www.globaltraqsdev.com/api/auth/logout/', null, tokenConfig(getState))
+			.then(() => {
+				dispatch({ type: 'LOGOUT_USER_SUCCESS' });
+			})
+			.catch((err) => {
+				dispatch({ type: 'LOGOUT_FAIL' });
+			});
+	};
 };
 
-export const register = ({ username, email, password, confirmPassword }) => {
+// REGISTER NEW USER
+export const register = ({ username, email, confirmPassword }) => {
 	return (dispatch) => {
 		const user = {
 			username: username,
 			email: email,
-			password: password,
-			confirmPassword: confirmPassword,
+			password: confirmPassword,
 		};
 
 		axios
@@ -66,14 +68,29 @@ export const register = ({ username, email, password, confirmPassword }) => {
 				dispatch({ type: 'LOAD_PROFILE_SUCCESS', payload: res.data });
 			})
 			.catch((err) => {
-				dispatch({ type: 'REGISTER_USER_FAIL', payload: err.res.data });
+				dispatch({ type: 'REGISTER_USER_FAIL' });
 			});
 	};
 };
 
+export const tokenConfig = (getState) => {
+	const token = getState().authReducer.token;
+	const config = {
+		headers: {
+			'Content-Type': 'application/json',
+			'X-Arqive-Api-Key': '4BqxMFdJ.3caXcBkTUuLWpGrfbBDQYfIyBVKiEif1',
+		},
+	};
+
+	if (token) {
+		config.headers['Authorization'] = `Token ${token}`;
+	}
+	return config;
+};
+
 export const returnErrors = (msg, status) => {
-  return {
-    type: 'GET_ERRORS',
-    payload: { msg, status }
-  }
+	return {
+		type: 'GET_ERRORS',
+		payload: { msg, status },
+	};
 };
