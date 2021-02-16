d8595b8cd6268738d2125b60544a397e26bb1e4a
cpahed <55155486+cpahed@users.noreply.github.com>
2021-02-15 22:47:20 -0800

    Create messageReducer.js

---
+++ app/redux/reducers/messageReducer.js
@@ -0,0 +1,14 @@
+const initialState = {
+  message: '',
+};
+
+export default message = (state = initialState, action) => {
+  switch (action.type) {
+    case 'CREATE_MESSAGE':
+      return Object.assign({}, state, {
+        message: action.payload
+      });
+    default:
+      return state;
+  }
+}
