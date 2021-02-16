140cb331e05e124099f5eb547b05c32e8e66239a
cpahed <55155486+cpahed@users.noreply.github.com>
2021-02-15 23:21:55 -0800

    Update index.js

--- app/routing/index.js
+++ app/routing/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-import { FontAwesome5 } from '@expo/vector-icons';
+import { FontAwesome5, MaterialIcons } from '@expo/vector-icons';
 import { connect } from 'react-redux';
 
 import DarkMapScreen from '../screens/darkMapScreen';
@@ -18,14 +19,33 @@ import LoginRegisterScreen from '../screens/loginRegisterScreen';
 import LoginScreen from '../screens/loginScreen';
 import RegisterScreen from '../screens/registerScreen';
 import ProfileScreen from '../screens/profileScreen';
+import BadgeScreen from '../screens/badgeScreen';
+import FollowingProfileScreen from '../screens/followingProfileScreen';
 
-const MapStack = createStackNavigator();
-function MapStackScreen() {
+import AnonToggleSwitch from '../components/anonToggleSwitch';
+import ModalOpener from '../components/modalOpener';
+
+import ContactUsModal from '../modals/contactUsModal';
+import HelpAndHotlineModal from '../modals/helpAndHotlineModal';
+import SupportUsModal from '../modals/supportUsModal';
+
+const AnonMapStack = createStackNavigator();
+function AnonMapStackScreen() {
 	return (
-		<MapStack.Navigator screenOptions={{ headerShown: false }}>
-			<MapStack.Screen name='Map' component={DarkMapScreen} />
-			<MapStack.Screen name='Story' component={StoryScreen} />
-		</MapStack.Navigator>
+		<AnonMapStack.Navigator screenOptions={{ headerShown: false }}>
+			<AnonMapStack.Screen name='Map' component={ DarkMapScreen } />
+			<AnonMapStack.Screen name='Story' component={ StoryScreen } />
+		</AnonMapStack.Navigator>
+	);
+}
+
+const UserMapStack = createStackNavigator();
+function UserMapStackScreen() {
+	return (
+		<UserMapStack.Navigator screenOptions={{ headerShown: false }}>
+			<UserMapStack.Screen name='Map' component={ LightMapScreen } />
+			<UserMapStack.Screen name='Story' component={ StoryScreen } />
+		</UserMapStack.Navigator>
 	);
 }
 
@@ -33,19 +53,30 @@ const StoriesStack = createStackNavigator();
 function StoriesStackScreen() {
 	return (
 		<StoriesStack.Navigator screenOptions={{ headerShown: false }}>
-			<StoriesStack.Screen name='StoryList' component={StoryListScreen} />
-			<StoriesStack.Screen name='Story' component={StoryScreen} />
+			<StoriesStack.Screen name='StoryList' component={ StoryListScreen } />
+			<StoriesStack.Screen name='Story' component={ StoryScreen } />
 		</StoriesStack.Navigator>
 	);
 }
 
-const BookmarkStack = createMaterialTopTabNavigator();
-function BookmarkStackScreen() {
+const BookmarkedUsersStack = createStackNavigator();
+function BookmarkedUsersStackScreen() {
 	return (
-		<BookmarkStack.Navigator style={styles.container}>
-			<BookmarkStack.Screen name='Stories' component={BookmarkPostScreen} />
-			<BookmarkStack.Screen name='Users' component={BookmarkUserScreen} />
-		</BookmarkStack.Navigator>
+    <BookmarkedUsersStack.Navigator>
+      <BookmarkedUsersStack.Screen name='UserList' options={{header: () => null}} component={ BookmarkUserScreen } />
+      <BookmarkedUsersStack.Screen name='UserProfile' options={{header: () => null}} component={ FollowingProfileScreen } />
+      <BookmarkedUsersStack.Screen name ='Story' options={{header: () => null}} component={ StoryScreen } />
+    </BookmarkedUsersStack.Navigator>
+  );
+}
+
+const BookmarkedTopTab = createMaterialTopTabNavigator();
+function BookmarkedTopTabScreen() {
+	return (
+		<BookmarkedTopTab.Navigator style={styles.container}>
+			<BookmarkedTopTab.Screen name='Stories' component={ BookmarkPostScreen } />
+			<BookmarkedTopTab.Screen name='Users' component={ BookmarkedUsersStackScreen } />
+		</BookmarkedTopTab.Navigator>
 	);
 }
 
@@ -53,25 +84,57 @@ const LoginStack = createStackNavigator();
 function LoginStackScreen() {
 	return (
 		<LoginStack.Navigator screenOptions={{ headerShown: false }}>
-			<LoginStack.Screen name='Initial' component={LoginRegisterScreen} />
-			<LoginStack.Screen name='Login' component={LoginScreen} />
-			<LoginStack.Screen name='Register' component={RegisterScreen} />
+			<LoginStack.Screen name='Initial' component={ LoginRegisterScreen } />
+			<LoginStack.Screen name='Login' component={ LoginScreen } />
+			<LoginStack.Screen name='Register' component={ RegisterScreen } />
 		</LoginStack.Navigator>
 	);
 }
 
 const ProfileStack = createStackNavigator();
-function ProfileStackScreen() {
+function ProfileStackScreen({ navigation }) {
 	return (
-		<ProfileStack.Navigator screenOptions={{ headerShown: false }}>
-			<ProfileStack.Screen name='Map' component={LightMapScreen} />
-			<ProfileStack.Screen name='Story' component={StoryScreen} />
-			<ProfileStack.Screen name='Profile' component={ProfileScreen} />
+		<ProfileStack.Navigator>
+			<ProfileStack.Screen name='Profile' options={{ headerRight: () => (
+					<MaterialIcons 
+						name='menu' 
+						style={{paddingRight: 10 }} 
+						size={24} 
+						color='black' 
+						onPress={() => navigation.openDrawer()} 
+					/>
+				)}} 
+				component={ ProfileScreen } />
+			<ProfileStack.Screen name='Story' options={{ header: () => null }} component={ StoryScreen } />
+			<ProfileStack.Screen name='Badge' options={{ header: () => null }} component={ BadgeScreen } />
 		</ProfileStack.Navigator>
 	);
 }
 
-// Will display if user is anonymous
+const MenuDrawer = createDrawerNavigator();
+function MenuDrawerScreen() {
+	return (
+		<MenuDrawer.Navigator
+			drawerPosition='right'
+			drawerType='slide'
+			drawerStyle={{ width: '80%' }}
+			drawerContent={(props) => {
+				return (
+					<DrawerContentScrollView {...props}>
+						<AnonToggleSwitch {...props} />
+						<ModalOpener {...props} name='help & hotline' navigateTo={ HelpAndHotlineModal } />
+						<ModalOpener {...props} name='support us' navigateTo='SupportUsModal' />
+						<ModalOpener {...props} name='contact us' navigateTo='ContactUsModal' />
+						<ModalOpener {...props} name='log out' navigateTo='LogoutUserModal' />
+					</DrawerContentScrollView>
+				);
+			}}>
+				<MenuDrawer.Screen name='Home' component={ ProfileStackScreen } />
+		</MenuDrawer.Navigator>
+	);
+}
+
+// WILL DISPLAY IS USER IS ANONYMOUS
 const NeedAuthTab = createBottomTabNavigator();
 function NeedAuthTabScreen() {
 	return (
@@ -93,15 +156,15 @@ function NeedAuthTabScreen() {
 				},
 			})}
 		>
-			<NeedAuthTab.Screen name='Map' component={MapStackScreen} />
-			<NeedAuthTab.Screen name='Stories' component={StoriesStackScreen} />
-			<NeedAuthTab.Screen name='Post' component={StoryPostScreen} />
-			<NeedAuthTab.Screen name='Profile' component={LoginStackScreen} options={{ tabBarVisible: false }} />
+			<NeedAuthTab.Screen name='Map' component={ AnonMapStackScreen } />
+			<NeedAuthTab.Screen name='Stories' component={ StoriesStackScreen } />
+			<NeedAuthTab.Screen name='Post' component={ StoryPostScreen } />
+			<NeedAuthTab.Screen name='Profile' component={ LoginStackScreen } options={{ tabBarVisible: false }} />
 		</NeedAuthTab.Navigator>
 	);
 }
 
-// Will display is user has successfully logged in
+// WILL DISPLAY IF USER HAS SUCCESSFULLY LOGGED IN
 const AppTab = createBottomTabNavigator();
 function AppTabScreen() {
 	return (
@@ -123,10 +186,10 @@ function AppTabScreen() {
 				},
 			})}
 		>
-			<AppTab.Screen name='Map' component={ProfileStackScreen} />
-			<AppTab.Screen name='Bookmarks' component={BookmarkStackScreen} />
-			<AppTab.Screen name='Post' component={StoryPostScreen} />
-			<AppTab.Screen name='Profile' component={ProfileScreen} />
+			<AppTab.Screen name='Map' component={ UserMapStackScreen } />
+			<AppTab.Screen name='Bookmarks' component={ BookmarkedTopTabScreen } />
+			<AppTab.Screen name='Post' component={ StoryPostScreen } />
+			<AppTab.Screen name='Profile' component={ MenuDrawerScreen } />
 		</AppTab.Navigator>
 	);
 }
@@ -137,9 +200,9 @@ function StackScreen({ hasAuth }) {
 		<NavigationContainer>
 			<Stack.Navigator initialRouteName='Auth' screenOptions={{ headerShown: false }}>
 				{!hasAuth ? (
-					<Stack.Screen name='Auth' component={NeedAuthTabScreen} />
+					<Stack.Screen name='Auth' component={ NeedAuthTabScreen } />
 				) : (
-					<Stack.Screen name='App' component={AppTabScreen} />
+					<Stack.Screen name='App' component={ AppTabScreen } />
 				)}
 			</Stack.Navigator>
 		</NavigationContainer>
