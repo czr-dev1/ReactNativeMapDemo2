1c950db5d18523cf28a5c89b5e6aaf11fea7bc1f
cpahed <55155486+cpahed@users.noreply.github.com>
2021-02-15 22:50:59 -0800

    Update rootReducer.js

--- app/redux/reducers/rootReducer.js
+++ app/redux/reducers/rootReducer.js
@@ -3,11 +3,13 @@ import { combineReducers } from 'redux';
 import stories from './storyReducer';
 import login from './authReducer';
 import profile from './profileReducer';
+import message from './messageReducer';
 
 const rootReducer = combineReducers({
   storyReducer: stories,
-  authReducer: login,
+  authReducer: auth,
   profileReducer: profile,
+  messageReducer: message,
 })
 
 export default rootReducer;
