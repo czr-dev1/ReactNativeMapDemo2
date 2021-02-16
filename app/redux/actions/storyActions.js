6f8ac4be379a47a0338e9a8f563686e60ad37548
cpahed <55155486+cpahed@users.noreply.github.com>
2021-02-15 22:30:54 -0800

    Update storyActions.js

--- app/redux/actions/storyActions.js
+++ app/redux/actions/storyActions.js
@@ -1,19 +1,20 @@
 import axios from 'axios';
 
+const config = {
+  headers: {
+    "X-Arqive-Api-Key": "4BqxMFdJ.3caXcBkTUuLWpGrfbBDQYfIyBVKiEif1",
+  },
+};
+
 export const loadStories = () => {
   return (dispatch) => {
     dispatch({ type: 'LOAD_STORIES_START' });
-    const config = {
-      headers: {
-        "X-Arqive-Api-Key": "4BqxMFdJ.3caXcBkTUuLWpGrfbBDQYfIyBVKiEif1",
-      },
-    };
 
-    axios.get('http://www.globaltraqsdev.com/api/pins', config)
+    axios.get(`http://www.globaltraqsdev.com/api/pins`, config)
     .then((res) => {
       dispatch({ type: 'LOAD_STORIES_SUCCESS', payload: res.data});
     }).catch((err) => {
-      dispatch({ type: 'LOAD_STORIES_FAILURE', payload: error});
+      dispatch({ type: 'LOAD_STORIES_FAILURE', payload: err.response.data});
     })
   }
 }
