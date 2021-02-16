fe73ef6cae770327c91f532cc1afdc2da0e85ba7
cpahed <55155486+cpahed@users.noreply.github.com>
2021-02-15 22:50:03 -0800

    Update profileReducer.js

--- app/redux/reducers/profileReducer.js
+++ app/redux/reducers/profileReducer.js
@@ -2,14 +2,25 @@ let initialState = {
 	profileData: [],
 	isLoading: true,
 	error: null,
+	users: '',
 };
 
 export default profile = (state = initialState, action) => {
 	switch (action.type) {
+		case 'LOAD_PROFILE_START':
+			return Object.assign({}, state, {
+				isLoading: true,
+			});
 		case 'LOAD_PROFILE_SUCCESS':
 			console.log('profile load successful');
 			return Object.assign({}, state, {
 				profileData: action.payload,
+				isLoading: false,
+			});
+		case 'LOAD_PROFILE_FAILURE':
+			console.log('profile load failed', action.payload);
+			return Object.assign({}, state, {
+				error: action.payload,
 				isLoading: false,
 			});
 		default:
