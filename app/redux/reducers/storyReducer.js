fcf70e35df0124dcd000b4e592e145e3fdf37317
cpahed <55155486+cpahed@users.noreply.github.com>
2021-02-15 22:51:48 -0800

    Update storyReducer.js

--- app/redux/reducers/storyReducer.js
+++ app/redux/reducers/storyReducer.js
@@ -7,11 +7,19 @@ let initialState = {
 export default stories = (state = initialState, action) => {
 	switch (action.type) {
 		case 'LOAD_STORIES_START':
-			return Object.assign({}, state, { isLoading: true });
+			return Object.assign({}, state, { 
+				isLoading: true 
+			});
 		case 'LOAD_STORIES_SUCCESS':
-			return Object.assign({}, state, { storyList: action.payload, isLoading: false });
+			return Object.assign({}, state, { 
+				storyList: action.payload, 
+				isLoading: false 
+			});
 		case 'LOAD_STORIES_FAILURE':
-			return Object.assign({}, state, { storyList: action.payload, isLoading: false });
+			return Object.assign({}, state, { 
+				storyList: action.payload, 
+				isLoading: false 
+			});
 		default:
 			return state;
 	}
