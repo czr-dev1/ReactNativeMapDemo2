86e43ea25ee808da7d64d09456b3e34ed667254d
cpahed <55155486+cpahed@users.noreply.github.com>
2021-02-15 22:18:14 -0800

    added user self delete action

--- app/redux/actions/authActions.js
+++ app/redux/actions/authActions.js
@@ -1,76 +1,10 @@
 import axios from 'axios';
+import { returnErrors } from './messageActions';
 
 const config = {
 	headers: {
 		'X-Arqive-Api-Key': '4BqxMFdJ.3caXcBkTUuLWpGrfbBDQYfIyBVKiEif1',
 	},
-};
-
-// LOAD ALL USER
-export const loadUsers = () => {
-	return (dispatch) => {
-		axios
-			.get('https://www.globaltraqsdev.com/auth/users', config)
-			.then((res) => {
-				dispatch({ type: 'LOAD_USERS', payload: res.data});
-			})
-			.catch ();
-	}
-};
-
-// LOGIN USER
-export const login = ({ username, password }) => {
-	return (dispatch) => {
-		const user = {
-			username: username,
-			password: password,
-		};
-
-		axios
-			.post('https://www.globaltraqsdev.com/api/auth/login', user, config)
-			.then((res) => {
-				dispatch({ type: 'LOGIN_USER_SUCCESS', payload: res.data });
-				dispatch({ type: 'LOAD_PROFILE_SUCCESS', payload: res.data });
-			})
-			.catch((err) => {
-				dispatch({ type: 'LOGIN_USER_FAIL' });
-			});
-	};
-};
-
-// LOGOUT USER
-export const logout = () => {
-	return (dispatch, getState) => {
-		axios
-			.post('https://www.globaltraqsdev.com/api/auth/logout/', null, tokenConfig(getState))
-			.then(() => {
-				dispatch({ type: 'LOGOUT_USER_SUCCESS' });
-			})
-			.catch((err) => {
-				dispatch({ type: 'LOGOUT_FAIL' });
-			});
-	};
-};
-
-// REGISTER NEW USER
-export const register = ({ username, email, confirmPassword }) => {
-	return (dispatch) => {
-		const user = {
-			username: username,
-			email: email,
-			password: confirmPassword,
-		};
-
-		axios
-			.post('https://www.globaltraqsdev.com/api/auth/register', user, config)
-			.then((res) => {
-				dispatch({ type: 'REGISTER_USER_SUCCESS', payload: res.data });
-				dispatch({ type: 'LOAD_PROFILE_SUCCESS', payload: res.data });
-			})
-			.catch((err) => {
-				dispatch({ type: 'REGISTER_USER_FAIL' });
-			});
-	};
 };
 
 export const tokenConfig = (getState) => {
@@ -88,9 +22,92 @@ export const tokenConfig = (getState) => {
 	return config;
 };
 
-export const returnErrors = (msg, status) => {
-	return {
-		type: 'GET_ERRORS',
-		payload: { msg, status },
+// GET ALL USERS
+export const getUsers = () => {
+	return (dispatch) => {
+		axios
+			.get('https://www.globaltraqsdev.com/auth/users', config)
+			.then((res) => {
+				dispatch({ type: 'GET_USERS', payload: res.data});
+			})
+			.catch ();
+	}
+};
+
+// LOGIN USER
+export const login = ({ username, password }) => {
+	return (dispatch) => {
+		const user = {
+			username: username,
+			password: password,
+		};
+		dispatch({ type: 'LOAD_PROFILE_START' });
+
+		axios
+			.post('https://www.globaltraqsdev.com/api/auth/login', user, config)
+			.then((res) => {
+				dispatch({ type: 'LOGIN_USER_SUCCESS', payload: res.data });
+				dispatch({ type: 'LOAD_PROFILE_SUCCESS', payload: res.data });
+			})
+			.catch((err) => {
+				dispatch({ type: 'LOGIN_USER_FAIL', payload: err.response.data });
+			});
+	};
+};
+
+// LOGOUT USER
+export const logout = () => {
+	return (dispatch, getState) => {
+		axios
+			.post('https://www.globaltraqsdev.com/api/auth/logout/', null, tokenConfig(getState))
+			.then(() => {
+				dispatch({ type: 'LOGOUT_USER_SUCCESS' });
+			})
+			.catch((err) => {
+				dispatch({ type: 'LOGOUT_FAIL' });
+				dispatch(returnErrors(err.response.data, err.response.status));
+			});
+	};
+};
+
+// REGISTER NEW USER
+export const register = ({ username, email, confirmPassword }) => {
+	return (dispatch) => {
+		const user = {
+			username: username,
+			email: email,
+			password: confirmPassword,
+		};
+		dispatch({ type: 'LOAD_PROFILE_START' });
+
+		axios
+			.post('https://www.globaltraqsdev.com/api/auth/register', user, config)
+			.then((res) => {
+				dispatch({ type: 'REGISTER_USER_SUCCESS', payload: res.data });
+				dispatch({ type: 'LOAD_PROFILE_SUCCESS', payload: res.data });
+			})
+			.catch((err) => {
+				dispatch({ type: 'REGISTER_USER_FAIL', payload: err.response.data });
+			});
+	};
+};
+
+// USER SELF DELETES ACCOUNT
+export const userSelfDelete = () => {
+	return (dispatch, getState) => {
+		axios
+			.delete(`https://www.globaltraqsdev.com/api/auth/user`,  tokenConfig(getState))
+			.then((res) => {
+				dispatch({ type: 'USER_SELF_DELETE', payload: res.data });
+			})
+			.catch((err) => {
+				dispatch(returnErrors(err.response.data, err.response.status));
+			});
+	};
+};
+
+export const setPrivacyMode = (setting) => {
+	return (dispatch) => {
+		dispatch({ type: 'SET_PRIVACY_MODE', isPrivacyMode: setting });
 	};
 };
