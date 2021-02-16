f66e7e3bc8855ebbc0e0f87afc075f8436293762
cpahed <55155486+cpahed@users.noreply.github.com>
2021-02-15 23:00:31 -0800

    Create contactUsModal.js

---
+++ app/modals/contactUsModal.js
@@ -0,0 +1,188 @@
+import axios from 'axios';
+import React, { useState } from 'react';
+import {
+	Dimensions,
+	ScrollView,
+	StyleSheet,
+	Text,
+	TextInput,
+	TouchableOpacity,
+	View,
+} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { FontAwesome5 } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { connect } from 'react-redux';
+
+import colors from '../config/colors';
+
+function ContactUsModal() {
+	const [email, setEmail] = useState('');
+	const [message, setMessage] = useState('');
+
+	const onSubmit = (e) => {
+		if (email !== '') {
+			const config = {
+				headers: {
+					'Content-Type': 'application/json',
+					'X-Arqive-Api-Key': '4BqxMFdJ.3caXcBkTUuLWpGrfbBDQYfIyBVKiEif1',
+				},
+			};
+			// Request Body
+			// const body = JSON.stringify({ username, email, password });
+			let data = JSON.stringify({ email: email, message: message });
+			axios
+				.post('https://globaltraqsdev.com/api/contactUs/', data, config)
+				.then((response) => {
+					setEmail('');
+					setMessage('');
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+		} else {
+			const config = {
+				headers: {
+					'X-Arqive-Api-Key': '4BqxMFdJ.3caXcBkTUuLWpGrfbBDQYfIyBVKiEif1',
+				},
+			};
+			setEmail(`Anonymous@anon.com`);
+
+			axios
+				.post('https://globaltraqsdev.com/api/contactUs/',
+					{
+						email: email,
+						message: message,
+					}, 
+          config
+				)
+				.then((response) => {
+					setEmail('');
+					setMessage('');
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+		}
+	};
+
+	return (
+		<SafeAreaView style={styles.container}>
+			<ScrollView style={{ width: '100%' }}>
+				<View style={{ flexDirection: 'row', justifyContent: 'center' }}>
+					<Text
+						style={{
+							fontSize: 24,
+							paddingTop: 24,
+							paddingBottom: 72,
+							color: '#787878',
+							fontWeight: 'bold',
+						}}
+					>
+						contact us
+					</Text>
+				</View>
+				<View style={styles.box}>
+					<Text style={{ fontSize: 22, paddingTop: 12, color: '#787878', fontWeight: 'bold' }}>
+						what's on your mind?
+					</Text>
+					<TextInput style={styles.input} placeholder='email (optional)' />
+					<TextInput style={styles.input} placeholder='subject' />
+					<TextInput style={styles.inputBorderless} multiline placeholder='message' />
+				</View>
+			</ScrollView>
+
+			<View style={{ flexDirection: 'column', flex: 1, width: '80%' }}>
+				<TouchableOpacity
+					style={{
+						alignSelf: 'flex-start',
+						position: 'absolute',
+						bottom: 35,
+						borderRadius: 5,
+						borderColor: '#ddd',
+						borderWidth: 2,
+					}}
+				>
+					<Text
+						style={{
+							paddingTop: 9,
+							paddingBottom: 9,
+							paddingLeft: 18,
+							paddingRight: 18,
+							color: '#919191',
+						}}
+					>
+						cancel
+					</Text>
+				</TouchableOpacity>
+				<TouchableOpacity
+					style={{
+						alignSelf: 'flex-end',
+						position: 'absolute',
+						bottom: 35,
+						borderRadius: 5,
+						borderColor: '#ddd',
+						borderWidth: 2,
+					}}
+					onPress={(e) => {
+						onSubmit(e);
+					}}
+				>
+					<Text
+						style={{
+							paddingTop: 9,
+							paddingBottom: 9,
+							paddingLeft: 18,
+							paddingRight: 18,
+							color: '#919191',
+						}}
+					>
+						send
+					</Text>
+				</TouchableOpacity>
+			</View>
+		</SafeAreaView>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: colors.white,
+		alignItems: 'center',
+		height: '100%',
+		width: Dimensions.get('window').width,
+	},
+	box: {
+		borderWidth: 2,
+		borderColor: '#ddd',
+		borderRadius: 69,
+		paddingTop: 18,
+		paddingBottom: 18,
+		paddingRight: 32,
+		paddingLeft: 32,
+		margin: 6,
+		alignItems: 'center',
+	},
+	input: {
+		borderBottomWidth: 1,
+		borderColor: '#ddd',
+		padding: 10,
+		fontSize: 14,
+		width: '100%',
+	},
+	inputBorderless: {
+		borderColor: '#ddd',
+		padding: 10,
+		fontSize: 14,
+		width: '100%',
+	},
+});
+
+const mapStateToProps = (state) => {
+	return {
+		isPrivacyMode: state.authReducer.isPrivacyMode,
+	};
+};
+
+export default connect(mapStateToProps, null)(ContactUsModal);
