eb6d587fc7e41f3fcafff69e16037b0e0aed1fea
cpahed <55155486+cpahed@users.noreply.github.com>
2021-02-15 22:57:25 -0800

    Create userList.js

---
+++ app/components/userList.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import {
+	Dimensions,
+	FlatList,
+	StyleSheet,
+	TouchableWithoutFeedback,
+	View,
+} from 'react-native';
+import { Card } from 'react-native-elements';
+import { useNavigation } from '@react-navigation/native';
+
+import colors from '../config/colors';
+
+const Item = ({ item }) => {
+	const navigation = useNavigation(); //There was a bug that made me have to import this
+	//if not imported and used it will just crash
+	//keep that in mind if props.navigation.navigate is not working
+	return (
+		<TouchableWithoutFeedback
+			key={item.id}
+			onPress={() => {
+				navigation.navigate('UserProfile');
+			}}
+		>
+			<Card>
+				<Card.Title>{item.title}</Card.Title>
+			</Card>
+		</TouchableWithoutFeedback>
+	);
+};
+
+function UserList(props) {
+	const renderItem = ({ item }) => {
+		const backgroundColor = 'gray';
+		return <Item item={item} props={props} style={{ backgroundColor }} />;
+	};
+
+	return (
+		<View>
+			<FlatList
+				style={styles.cardContainer}
+				data={props.users}
+				renderItem={renderItem}
+				keyExtractor={(item) => '' + item.id}
+			/>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	cardContainer: {
+		width: Dimensions.get('window').width,
+	},
+});
+
+export default UserList;
