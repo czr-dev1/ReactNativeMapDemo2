0d10857aa6e8e9ab4a6121f94c0595cc776c9910
cpahed <55155486+cpahed@users.noreply.github.com>
2021-02-15 22:31:45 -0800

    Update messageActions.js

--- app/redux/actions/messageActions.js
+++ app/redux/actions/messageActions.js
@@ -3,13 +3,13 @@ export const createMessage = (msg) => {
   return {
     type: 'CREATE_MESSAGE',
     payload: msg
-  };
-};
+  }
+}
 
 // RETURN ERRORS
 export const retrunErrors = (msg, status) => {
   return {
     type: 'GET_ERRORS',
     payload: { msg, status }
-  };
-};
+  }
+}
