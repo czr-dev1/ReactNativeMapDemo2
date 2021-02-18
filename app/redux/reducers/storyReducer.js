251434134d200d6d8c1e4d4721a5055bbe449ec9
Carlos <carloslariossolis@gmail.com>
2021-02-17 23:15:13 -0800

    can make story private

--- app/redux/reducers/storyReducer.js
+++ app/redux/reducers/storyReducer.js
@@ -15,6 +15,15 @@ export default stories = (state = initialState, action) => {
     case 'LOAD_STORIES_FAILURE':
       console.log('failed to load stories');
       return Object.assign({}, state, { storyList: action.payload, isLoading: false });
+    case 'MAKE_STORY_PRIVATE_START':
+      console.log('loading stories');
+      return Object.assign({}, state, { isLoading: true });
+    case 'MAKE_STORY_PRIVATE_SUCCESS':
+      console.log('successfully loaded stories');
+      return Object.assign({}, state, { isLoading: false });
+    case 'MAKE_STORY_PRIVATE_FAILURE':
+      console.log('failed to load stories');
+      return Object.assign({}, state, { isLoading: false });
     default:
       return state
   }
