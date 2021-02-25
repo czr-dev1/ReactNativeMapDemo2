9672ac460e7b4df8d8191771db97a63525d4283d
Carlos <carloslariossolis@gmail.com>
2021-02-24 17:16:21 -0800

    display comments and story color

--- app/screens/storyScreen.js
+++ app/screens/storyScreen.js
@@ -9,17 +9,26 @@ import {
   TouchableOpacity,
   TextInput,
   View,
-  ScrollView
+  ScrollView,
+  Button
 } from 'react-native';
 import MapView from 'react-native-map-clustering';
 import { connect } from 'react-redux';
-import { FontAwesome5, MaterialIcons } from '@expo/vector-icons';
+import { FontAwesome5, MaterialIcons, FontAwesome } from '@expo/vector-icons';
 
 import colors from '../config/colors';
 
 function storyScreen(props) {
   const { title, description, id } = props.route.params;
   const [story, setStory] = useState({});
+  const [comments, setComments] = useState([{
+      "commenter": 0,
+      "description": "",
+      "id": 0,
+      "is_anonymous_comment": false,
+      "pin": 0,
+      "username": "",
+    }]);
 
   useEffect(() => {
     getStory();
@@ -32,6 +41,7 @@ function storyScreen(props) {
       console.log(item.id);
     })
     setStory(tempStory[0]);
+    setComments(tempStory[0].commentstory);
   }
 
 
@@ -39,43 +49,65 @@ function storyScreen(props) {
 
   return(
     <SafeAreaView style={styles.container}>
-      <ScrollView style={{width: '100%'}}>
-      <View style={{width: '100%', padding: '10%'}}>
-        <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
-          <View style={{flexDirection: 'row', alignItems: 'center'}}>
-            <MaterialIcons style={{borderRadius: 50, padding: 5, backgroundColor: 'gray', transform: [{rotateY: '180deg'}]}} name="chat-bubble-outline" size={32} color="white" />
-            <Text style={{ paddingLeft: 5, color: '#787878', fontSize: 18, fontWeight: 'bold'}}>{story.is_anonymous_pin ? "anonymous" : story.username}</Text>
-          </View>
-          <FontAwesome5 name="bookmark" size={24} color="black" />
-        </View>
-        <View style={{paddingLeft: '5%'}}>
-          <Text style={{color: '#787878', fontWeight: 'bold', fontSize: 24}}>{story.title}</Text>
-          {story.address === "" ? null : <Text style={{color: '#787878', fontWeight: 'bold'}}>{story.address}</Text>}
-          <View style={{flexDirection: 'row', paddingBottom: 5}}>
-            {story.locality === "" ? null : <Text style={{color: '#787878', fontWeight: 'bold'}}>{story.locality}, </Text>}
-            {story.region === "" ? null : <Text style={{color: '#787878', fontWeight: 'bold'}}>{story.region}</Text>}
-          </View>
-          <Text style={{paddingBottom: 5}}>{
-            story.startDate === null ? story.postDate : story.endDate === null ? story.startDate : story.startDate + " - " + story.endDate
-          }</Text>
-          <Text>{story.description}</Text>
-        </View>
-        <TouchableOpacity style={{borderRadius: 5, borderColor: '#ddd', borderWidth: 2}}>
-          <Text style={{paddingTop: 9, paddingBottom: 9, paddingLeft: 18, paddingRight: 18, color: '#919191'}}>flag</Text>
-        </TouchableOpacity>
-      </View>
-      <View style={{width: '100%', padding: '7%'}}>
-        <TextInput
-          style={styles.box}
-          multiline
-          placeholder='enter comment'
-        />
-        <TouchableOpacity style={{borderRadius: 5, borderColor: '#ddd', borderWidth: 2}}>
-          <Text style={{paddingTop: 9, paddingBottom: 9, paddingLeft: 18, paddingRight: 18, color: '#919191'}}>comment</Text>
-        </TouchableOpacity>
-      </View>
+      <ScrollView style={{width: '100%', backgroundColor: (story.category === 1 ? "#e01784" : story.category == 2 ? "#00ce7d" : "#248dc1")}}>
+        <View style={{padding: '20%'}}></View>
 
+        <View style={{width: '100%', paddingTop: '10%', paddingLeft: '10%', paddingRight: '10%', backgroundColor: 'white', borderTopLeftRadius: 40, borderTopRightRadius: 40}}>
+          <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+            <View style={{flexDirection: 'row', alignItems: 'center'}}>
+              <MaterialIcons style={{borderRadius: 50, padding: 5, backgroundColor: 'gray', transform: [{rotateY: '180deg'}]}} name="chat-bubble-outline" size={32} color="white" />
+              <Text style={{ paddingLeft: 5, color: '#787878', fontSize: 18, fontWeight: 'bold'}}>{story.is_anonymous_pin ? "anonymous" : story.username}</Text>
+            </View>
+            <FontAwesome5 name="bookmark" size={24} color="black" />
+          </View>
+          <View style={{paddingLeft: '5%'}}>
+            <Text style={{color: '#787878', fontWeight: 'bold', fontSize: 24}}>{story.title}</Text>
+            {story.address === "" ? null : <Text style={{color: '#787878', fontWeight: 'bold'}}>{story.address}</Text>}
+            <View style={{flexDirection: 'row', paddingBottom: 5}}>
+              {story.locality === "" ? null : <Text style={{color: '#787878', fontWeight: 'bold'}}>{story.locality}, </Text>}
+              {story.region === "" ? null : <Text style={{color: '#787878', fontWeight: 'bold'}}>{story.region}</Text>}
+            </View>
+            <Text style={{paddingBottom: 5}}>{
+              story.startDate === null ? story.postDate : story.endDate === null ? story.startDate : story.startDate + " - " + story.endDate
+            }</Text>
+            <Text>{story.description}</Text>
+            <View style={{flexDirection: 'row', paddingTop: 14, paddingBottom: 14}}>
+              <TouchableOpacity style={{borderRadius: 5, borderColor: '#ddd', borderWidth: 2}}>
+                <Text style={{paddingTop: 9, paddingBottom: 9, paddingLeft: 18, paddingRight: 18, color: '#919191'}}>flag</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
 
+        <View style={{width: '100%', paddingLeft: '7%', paddingRight: '10%', backgroundColor: 'white'}}>
+          <TextInput
+            style={styles.box}
+            multiline
+            placeholder='enter comment'
+          />
+          <View style={{flexDirection: 'row-reverse', padding: 14}}>
+            <TouchableOpacity style={{borderRadius: 5, borderColor: '#ddd', borderWidth: 2, width: 'auto'}}>
+              <Text style={{paddingTop: 9, paddingBottom: 9, paddingLeft: 18, paddingRight: 18, color: '#919191'}}>comment</Text>
+            </TouchableOpacity>
+          </View>
+          {
+            comments.map((comment, i) => {
+              return (
+                <View key={i} style={{justifyContent: 'space-between', borderTopWidth: 2, borderColor: '#ddd', padding: 14}}>
+                  <Text>{comment.description}</Text>
+                  <View style={{flexDirection: 'row-reverse', alignItems: 'flex-end'}}>
+                    <TouchableOpacity style={{padding: 14}}>
+                      <FontAwesome name="flag" size={24} color="black" />
+                    </TouchableOpacity>
+                    <TouchableOpacity style={{padding: 14}}>
+                      <FontAwesome name="thumbs-o-up" size={24} color="black" />
+                    </TouchableOpacity>
+                  </View>
+                </View>
+              )
+            })
+          }
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
