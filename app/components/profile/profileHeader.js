b2cdc4d2f728923831c314753806f7063e3fed49
cpahed <55155486+cpahed@users.noreply.github.com>
2021-05-05 19:16:26 -0700

    updated profile components

--- app/components/profile/profileHeader.js
+++ app/components/profile/profileHeader.js
@@ -1,16 +1,25 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Platform, View } from "react-native";
 import { connect } from "react-redux";
+import Text from "../text";
 
 import colors from "../../config/colors";
 
 function ProfileHeader(props) {
   return (
     <View>
-      <View
+      {/* determines if platform is android or ios 
+      to style header properly */}
+      {Platform.OS === 'android' ? (
+        <View
         style={{
+          position: "absolute",
           alignItems: "center",
           justifyContent: "center",
+          top: 0,
+          bottom: 0,
+          right: 0,
+          left: 0,
         }}
       >
         <Text
@@ -19,6 +28,27 @@ function ProfileHeader(props) {
           {props.username}
         </Text>
       </View>
+      ) : (
+        <View
+        style={{
+          alignItems: "center",
+          justifyContent: "center",
+          // comment these out for iOS
+          // position: "absolute",
+          //top: 0,
+          //bottom: 0,
+          //right: 0,
+          //left: 0,
+        }}
+      >
+        <Text
+          style={{ fontSize: 18, fontWeight: "bold", color: colors.purple }}
+        >
+          {props.username}
+        </Text>
+      </View>
+      )}
+      
     </View>
   );
 }
