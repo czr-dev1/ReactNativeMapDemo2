5cb50a85de12ef975491c4e2831e7b294e772422
StewartMckenzie <mckenzie.stewart.e@gmail.com>
2022-03-03 09:55:42 -0800

    Persist Code

--- App.js
+++ App.js
@@ -25,16 +25,29 @@ const Icon = createIconSetFromIcoMoon(
   "icomoon.ttf",
 );
 
+
+//User persistance 
+//Use asyncStorage as a store for the users state
 const persistConfig = {
   key: "root",
   storage: AsyncStorage,
 };
+
+
 // const store = createStore(rootReducer, applyMiddleware(thunk));
+
+//Create a persistor from the root reducer that holds the apps state
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
+//Use the new persistor(Really just a reducer) to manage the apps state
 const store = createStore(persistedReducer, applyMiddleware(thunk));
 let persistor = persistStore(store);
+
+//Clear Async Storage so that the apps state must be taken from our persistor
 AsyncStorage.clear();
+
+
+
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
     shouldShowAlert: true,
